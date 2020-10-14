b26a7f998c563f23d24884dcf5b70e1d91548c53
durongze <durongze@qq.com>
2020-10-14 21:07:57 +0800

    Update Program.cs

--- src/Test/Program.cs
+++ src/Test/Program.cs
@@ -7,7 +7,21 @@ namespace Haceau.Gooogle.Translate.Test
     class Program
     {
         private static readonly HttpClient client = new HttpClient();
+        
+        public static string GetPinYinBySentence(string sentence)
+        {
+            Translation t = new Translation(client);
+            String transResult = t.PinYin(sentence);
+            // zhuang
+            int wordNum = sentence.Length;
+            int wordPinYinLen = 6 * wordNum;
+            StringBuilder pinYin = new StringBuilder(wordPinYinLen);
 
+            int ret = GetPinYinXPtr(transResult, transResult.Length, pinYin, wordPinYinLen);
+
+            return pinYin.ToString();
+        }
+        
         public static void Main()
         {
             Translation t = new Translation(client);
